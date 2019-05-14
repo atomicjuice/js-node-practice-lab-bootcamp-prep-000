36063f739e7984d9a8969a6891cae99146bb7a2c
keemo <keemowatson1@gmail.com>
2019-05-14 12:23:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,4 @@ console.log(name)
 console.log("Height:")
 console.log(height+1)
 udkjndjhnsenn
+I just wrote this random stuff to test whether the lesson will still submit if done correctly but I added other stuff, if this is on my github.. then the answer is yes.
